c615899a0f1291b8da1a20287e868d009a63105a
Bretton Wade <bretton.wade@gmail.com>
2017-11-02 13:38:31 -0400

    update to separate the test harness and a test shim for jjs, and update the enum test code to remove the harness from it.

--- src/test/js/enum.js
+++ src/test/js/enum.js
@@ -1,21 +1,6 @@
 "use strict;" 
 
 //-----------------------------------------------------------------------------
-let Test = {
-    assertTrue : function (message, test) {
-        console.log (((test === true) ? "PASS" : "FAIL") + " - " + message);
-        if (test === false) {
-			if (typeof (exit) !== "undefined") {
-				exit (1);
-			}
-        }
-    },
-    assertFalse : function (message, test) {
-		Test.assertTrue (message, test === false);
-    }
-};
-
-//-----------------------------------------------------------------------------
 // test functions built on the harness
 //-----------------------------------------------------------------------------
 let testEnum = function () {
